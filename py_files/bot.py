3701d8fd227bd0a18ddb0265cb22855c6417dc30
XAREKUP <dadasdadas234@gmail.com>
2024-11-16 09:39:34 +0000

    add all users message

--- py_files/bot.py
+++ py_files/bot.py
@@ -4,6 +4,7 @@ import telebot
 import time
 import datetime
 import math
+import os
 
 bot = telebot.TeleBot("TOKEN") #токен бота
 commands_switch = {'start_server' :["sh sh_scripts/start_server.sh", "time_out_on", "all_users_message_on"],
@@ -12,6 +13,8 @@ commands_switch = {'start_server' :["sh sh_scripts/start_server.sh", "time_out_o
 
 time_last_call = datetime.datetime(2011,11,11,11,11)
 time_out=5.0
+
+users_filename = 'users.txt'
 
 def command_executer(message):
    global time_last_call
@@ -26,9 +29,22 @@ def command_executer(message):
                time_last_call = datetime.datetime.now()
             else:
                bot.send_message(message.chat.id, "Timeout: " + str(round(time_diff, 1)) + "/" + str(time_out))
-
          else:
             bot.send_message(message.chat.id, check_output(commands_switch[command][0], shell = True))
+
+         if(commands_switch[command][2] == "all_users_message_on" and \
+            not (commands_switch[command][1] == "time_out_on" and time_diff < time_out)):
+            file_str = open(users_filename, 'r')
+            lines = file_str.readlines()
+            #bot.send_message(message.chat.id, str(lines))
+            file_str.close()
+
+            for line in lines:
+               id = int(line.split()[0])
+               if(id == id): #!= message.chat.id):
+                  str_text = line.split()[3] + ": " + command
+                  bot.send_message(id, str_text)
+
       except:
          bot.send_message(message.chat.id, 'error: ' + command) #если команда некорректна
 
@@ -36,6 +52,24 @@ def command_executer(message):
 ########BOT HELP########
 @bot.message_handler(commands=['start'])
 def start_bot(message):
+   if(os.path.isfile(users_filename) == False):
+      with open(users_filename, 'w') as fp:
+         pass
+
+   file_str = open(users_filename, 'r')
+   lines = file_str.readlines()
+   #bot.send_message(message.chat.id, str(lines))
+   file_str.close()
+
+   str_user = str(message.chat.id) + ' ' + message.from_user.first_name \
+              + ' ' + message.from_user.last_name + ' ' +message.from_user.username + '\n'
+
+   #bot.send_message(message.chat.id, str_user)
+   if((str_user in lines) == False):
+      file_str = open(users_filename, 'a')
+      file_str.write(str_user)
+      file_str.close()
+
    help_bot(message)
 
 @bot.message_handler(commands=['help'])
---
+++ test.py
@@ -0,0 +1,3 @@
+file_str = open('users.txt', 'a')
+file_str.write('test' + '\n')
+file_str.close()

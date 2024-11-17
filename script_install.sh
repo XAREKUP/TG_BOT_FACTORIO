158f2064fe72422a08191682348923b704b8bc0a
XAREKUP <dadasdadas234@gmail.com>
2024-11-17 08:49:49 +0000

    script_install.sh update: parameters

--- script_install.sh
+++ script_install.sh
@@ -1,27 +1,67 @@
 #!/bin/bash
 echo "****************START****************"
 
+# Установка зависимостей
+sudo apt install unzip
+sudo apt install pip
+sudo pip install pytelegrambotapi --break-system-packages
+
+# Запрос значений параметров у пользователя
+read -p "Enter your Telegram token: " tg_token
+http_code=$(curl -o /dev/null 2>/dev/null -w '%{http_code}\n' https://api.telegram.org/bot${tg_token}/getMe)
+
+if [ "$http_code" = "200" ]; then
+    echo "The Telegram token is correct"
+else
+    echo "There is something wrong with the token. Code: ${http_code}"
+fi
+
+
+read -p "Enter the waiting time (in seconds): " time_out
+read -p "Enter the name of the bot's user data file: " users_filename
+#read -p "IP-адрес для RCON: " rcon_ip
+read -p "Enter the password for RCON: " rcon_password
+read -p "Enter the port for RCON: " rcon_port
+
+# Сохранение параметров в файл
+echo "tg_token: ${tg_token}" > ./parameters.txt
+echo "time_out: ${time_out}" >> ./parameters.txt
+echo "users_filename: ${users_filename}" >> ./parameters.txt
+#echo "rcon_ip ${rcon_ip}" >> ./parameters.txt
+echo "rcon_password ${rcon_password}" >> ./parameters.txt
+echo "rcon_port ${rcon_port}" >> ./parameters.txt
+
+# Установка Factorio
 sudo wget -O factorio_headless.tar.gz https://www.factorio.com/get-download/2.0.15/headless/linux64
 sudo tar -xf factorio_headless.tar.gz
 sudo mv factorio /opt
 sudo rm factorio_headless.tar.gz
 
+# Создание директории для модов
+sudo mkdir /opt/factorio/mods
+
+# Изменение прав доступа к папке Factorio
+sudo chmod 777 /opt/factorio -R
+
+# Распаковка бота
+#wget https://github.com/XAREKUP/TG_BOT_FACTORIO/archive/refs/heads/main.zip
+#unzip main.zip -d ./tg_bot_factorio
+#rm main.zip
+
+# Настройка сервера Factorio
 sudo touch /etc/systemd/user/factorio_server.service
 echo "[Unit]
 Description=Factorio Headless Server
 
 [Service]
 Type=simple
-ExecStart=/opt/factorio/bin/x64/factorio --start-server /opt/factorio/saves/world.zip  --rcon-port 25575 --rcon-password '123'
+ExecStart=/opt/factorio/bin/x64/factorio --start-server /opt/factorio/saves/world.zip  --rcon-port ${rcon_port} --rcon-password '${rcon_password}'
 " | sudo tee /etc/systemd/user/factorio_server.service
 
-sudo /opt/factorio/bin/x64/factorio --create /opt/factorio/saves/world.zip
-sudo mkdir /opt/factorio/mods
-sudo chmod 777 /opt/factorio -R
+# Cоздание мира
+/opt/factorio/bin/x64/factorio --create /opt/factorio/saves/world.zip
 
+# Перезагрузка демона systemd
 systemctl --user daemon-reload
-
-sudo apt install pip
-sudo pip install pytelegrambotapi --break-system-packages
 
 echo "****************END****************"
